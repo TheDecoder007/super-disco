56c98dbd4cffef656d4402293d6a3b2e32504e8a
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-09 18:52:57 -0400

    fixed error

--- script.js
+++ script.js
@@ -4,56 +4,40 @@ var now = moment();
 var timeDisplay = document.getElementById('currentDay');
 timeDisplay.innerHTML = now.format("dddd, MMMM, Do");
 
-var tasks = ["", "", "", "", "", "", "", ""];
+var tasks = [];
 
 //save text to local storage
 
 document.getElementById('submitBtn-0').addEventListener("click", function(event) {
- console.log("we clicked");
     event.preventDefault();
-    console.log(document.getElementById('textArea-0').value)
   saveTasks(document.getElementById('textArea-0').value, 0)
 });
 document.getElementById('submitBtn-1').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-1').value)
    saveTasks(document.getElementById('textArea-1').value, 1)
  });
  document.getElementById('submitBtn-2').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-2').value)
    saveTasks(document.getElementById('textArea-2').value, 2)
  });
  document.getElementById('submitBtn-3').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-3').value)
    saveTasks(document.getElementById('textArea-3').value, 3)
  });
  document.getElementById('submitBtn-4').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-4').value)
    saveTasks(document.getElementById('textArea-4').value, 4)
  });
  document.getElementById('submitBtn-5').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-5').value)
    saveTasks(document.getElementById('textArea-5').value, 5)
  });
  document.getElementById('submitBtn-6').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-6').value)
    saveTasks(document.getElementById('textArea-6').value, 6)
  });
  document.getElementById('submitBtn-7').addEventListener("click", function(event) {
-  console.log("we clicked");
      event.preventDefault();
-     console.log(document.getElementById('textArea-7').value)
    saveTasks(document.getElementById('textArea-7').value, 7)
  });
 
@@ -62,8 +46,7 @@ document.getElementById('submitBtn-1').addEventListener("click", function(event)
 var getText = JSON.parse(localStorage.getItem("textArea"));
 
 var loadTasks = function() {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-console.log(tasks);
+ tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   
 
 for(var i = 0; i < tasks.length; i++) {
@@ -72,9 +55,7 @@ for(var i = 0; i < tasks.length; i++) {
 };
 
 var saveTasks = function(task, index) {
-  console.log(saveTasks);
   tasks[index] = task;
-  console.log(tasks);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
@@ -89,7 +70,6 @@ var auditTask = function(taskEl) {
     //get date from task element
         var date = $(taskEl).find("currentDay").text().trim();
     
-        console.log(date);
     //convert to moment object at 5:00pm
        var time = moment(date, "L").set("hour", 17)
     //remove any old classes from element
